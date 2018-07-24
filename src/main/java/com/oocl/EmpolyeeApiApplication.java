eaf4651acea1160a35c527bd7308348e4ec18888
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-24 17:20:14 +0800

    change data

--- src/main/java/com/oocl/EmpolyeeApiApplication.java
+++ src/main/java/com/oocl/EmpolyeeApiApplication.java
@@ -18,11 +18,11 @@ public class EmpolyeeApiApplication {
 	@Bean
 	public static List<Employee> allEmployee(){
 		List<Employee> employeeList = new ArrayList<>();
-		employeeList.add(new Employee(0,"小明",20,"男"));
-		employeeList.add(new Employee(1,"小红",19,"女"));
-		employeeList.add(new Employee(2,"小智",15,"男"));
-		employeeList.add(new Employee(3,"小刚",16,"男"));
-		employeeList.add(new Employee(4,"小霞",15,"女"));
+		employeeList.add(new Employee(1,"小明",20,"男"));
+		employeeList.add(new Employee(2,"小红",19,"女"));
+		employeeList.add(new Employee(3,"小智",15,"男"));
+		employeeList.add(new Employee(4,"小刚",16,"男"));
+		employeeList.add(new Employee(5,"小霞",15,"女"));
 		return employeeList;
 	}
 }
---
+++ src/main/java/com/oocl/dao/employeeRepository.java
@@ -0,0 +1,7 @@
+package com.oocl.dao;
+
+import com.oocl.model.Employee;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+public interface employeeRepository  {
+}
